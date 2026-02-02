9e7d8d884b141d3e14240353b04ea88cfd5ebb65
Megha Wadhwa <meghawadhwa20@gmail.com>
2026-02-02 22:44:45 +0530

    feat: Implemented login UI

--- pet_sure/lib/core/app_theme.dart
+++ pet_sure/lib/core/app_theme.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
-    fontFamily: 'PlusJakartaSans'
+    fontFamily: 'PlusJakartaSans',
   );
 
+  // Text       → #374151
+  // Hint       → #9CA3AF
+  // Border     → #E5E7EB
   static const Color primaryOrange = Color(0xFFE67E22);
+  static const Color primaryGray = Color(0xFF374151);
+  static const Color secondaryGray = Color(0xFF6B7280);
+  static const Color tertiaryGray = Color(0xFF9CA3AF);
+  static const Color borderGray = Color(0xFFE5E7EB);
+  static const Color background = Color(0xFFFAFAF9);
 }
-
---
+++ pet_sure/lib/screens/login_screen.dart
@@ -0,0 +1,248 @@
+import 'package:flutter/material.dart';
+import 'package:pet_sure/core/app_theme.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          padding: const EdgeInsets.all(24),
+          color: AppTheme.background,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // const SizedBox(height: 60),
+              Column(
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.yellow.withValues(alpha: 0.12),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Icon(
+                      Icons.pets,
+                      size: 32,
+                      color: AppTheme.primaryOrange,
+                    ),
+                  ),
+
+                  const SizedBox(height: 32),
+                  const Text(
+                    'Welcome Back',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                  ),
+
+                  const SizedBox(height: 12),
+                  const Text(
+                    'Discover the best care for your furry companions',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      height: 1.5,
+                      color: AppTheme.secondaryGray,
+                    ),
+                  ),
+                ],
+              ),
+
+              // const SizedBox(height: 12),
+              const LoginForm(),
+
+              // const SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'New to PetCare?',
+                    style: TextStyle(
+                      color: AppTheme.secondaryGray,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(width: 2),
+                  GestureDetector(
+                    onTap: () {
+                      // later
+                    },
+                  child: Text(
+                    'Create an account',
+                    style: TextStyle(
+                      color: AppTheme.primaryOrange,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  bool _obscurePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        /// EMAIL LABEL
+        const Text(
+          'EMAIL ADDRESS',
+          style: TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.w600,
+            color: AppTheme.primaryGray,
+            letterSpacing: 1,
+          ),
+        ),
+
+        const SizedBox(height: 8),
+
+        /// EMAIL FIELD
+        TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'name@example.com',
+            hintStyle: const TextStyle(color: AppTheme.tertiaryGray),
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 16,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(28),
+              borderSide: const BorderSide(color: AppTheme.borderGray),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(28),
+              borderSide: BorderSide(
+                color: AppTheme.primaryOrange.withValues(
+                  alpha: 0.4,
+                ), // primary orange
+              ),
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 20),
+
+        /// PASSWORD LABEL + FORGOT
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'PASSWORD',
+              style: TextStyle(
+                fontSize: 13,
+                fontWeight: FontWeight.w600,
+                color: AppTheme.primaryGray,
+                letterSpacing: 1,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                // later
+              },
+              child: Text(
+                'Forgot?',
+                style: TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600,
+                  color: AppTheme.primaryOrange,
+                ),
+              ),
+            ),
+          ],
+        ),
+
+        const SizedBox(height: 8),
+
+        /// PASSWORD FIELD
+        TextField(
+          obscureText: _obscurePassword,
+          decoration: InputDecoration(
+            hintText: 'Enter your password',
+            hintStyle: const TextStyle(color: AppTheme.tertiaryGray),
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 16,
+            ),
+            suffixIcon: IconButton(
+              icon: Icon(
+                _obscurePassword ? Icons.visibility_off : Icons.visibility,
+                color: AppTheme.tertiaryGray,
+              ),
+              onPressed: () {
+                setState(() {
+                  _obscurePassword = !_obscurePassword;
+                });
+              },
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(28),
+              borderSide: const BorderSide(color: AppTheme.borderGray),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(28),
+              borderSide: BorderSide(
+                color: AppTheme.primaryOrange.withValues(alpha: 0.4),
+              ),
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 24),
+
+        SizedBox(
+          width: double.infinity,
+          height: 52,
+          child: ElevatedButton(
+            onPressed: () {
+              // TODO: handle login later
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: AppTheme.primaryOrange,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(28),
+              ),
+            ),
+            child: const Text(
+              'Log In',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
